72ad5e35effb840fefddf273a23a964d81c7f1df
Robert de Vries <robert.de.vries@wearespindle.com>
2018-03-22 17:49:19 +0100

    Updated the documentation

--- django_logical_perms/loaders.py
+++ django_logical_perms/loaders.py
@@ -5,11 +5,22 @@ from django.conf import settings
 
 
 def load_all_permissions_modules():
+    """Go through all app configs and try loading their permissions module.
+
+    By loading the permissions modules, the permissions will automatically get registered
+    with the storage backend. This will automatically enable evaluation through Django's
+    has_perms function (if the custom auth backend is loaded).
+    """
+
     for _, app_config in apps.app_configs.items():
         load_permissions_module(app_config)
 
 
 def load_permissions_module(app_config):
+    """Loads the permissions module for the given app.
+
+    :param app_config: The app's AppConfig instance.
+    """
     try:
         # Import and only process apps with a permissions module
         permissions_module_name = getattr(settings, 'PERMISSIONS_MODULE_NAME', 'permissions')
