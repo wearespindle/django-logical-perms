69e8b8ae4fabfbc2b8037b70cba1b15c2ce8427f
Robert de Vries <robert.de.vries@wearespindle.com>
2018-03-23 16:00:24 +0100

    Implemented a mixin for Tastypie field-based permissions

---
+++ django_logical_perms/tastypie/mixins.py
@@ -0,0 +1,27 @@
+class FieldPermissionsMixin(object):
+    """Incorporate per object field-based permissions in Tastypie.
+
+    You can use this class as a mixin in your API or Resource classes to allow for compatibility with
+    object field-based permissions in your API.
+
+    Note:
+        You must specify ``field_permissions`` in the Meta class. You can use ``FieldPermissionsConfigSet``
+        class for this.
+    """
+    def update_bundle_fields(self, bundle, action):
+        allowed_fields = self.Meta.field_permissions.get_permitted_field_names(
+            action=action, user=bundle.request.user, obj=bundle.obj)
+
+        for field in bundle.data.keys():
+            if field not in allowed_fields:
+                del bundle.data[field]
+
+        return bundle
+
+    def hydrate(self, bundle):
+        return super(FieldPermissionsMixin, self).hydrate(
+            self.update_bundle_fields(bundle, action='change'))
+
+    def dehydrate(self, bundle):
+        return super(FieldPermissionsMixin, self).dehydrate(
+            self.update_bundle_fields(bundle, action='view'))
