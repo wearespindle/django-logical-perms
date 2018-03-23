de34268f6e9b66605cc9e5cd9bf609cf7a8d2abb
Robert de Vries <robert.de.vries@wearespindle.com>
2018-03-23 15:59:53 +0100

    Implemented an Authorization class for Tastypie that is compatible with object-level permissions

---
+++ django_logical_perms/tastypie/authorization.py
@@ -0,0 +1,14 @@
+from tastypie.authorization import DjangoAuthorization
+
+
+class DjangoObjectAuthorization(DjangoAuthorization):
+    """Authorization class that will add object-level permission checks to Tastypie's ``DjangoAuthorization`` class."""
+    def check_user_perm(self, user, permission, obj_or_list):
+        if isinstance(obj_or_list, list):
+            for obj in obj_or_list:
+                if not user.has_perm(permission, obj) and not user.has_perm(permission):
+                    return False
+
+            return True
+
+        return user.has_perm(permission, obj_or_list) or user.has_perm(permission)
