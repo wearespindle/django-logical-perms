fedc313939861c1193ae070acfbdad1b376410a4
Robert de Vries <robert.de.vries@wearespindle.com>
2018-04-09 15:48:07 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 from setuptools import setup
 
+__title__ = 'django-logical-perms'
+__author__ = 'Devhouse Spindle'
+__email__ = 'opensource@wearespindle.com'
+__version__ = '0.1'
+__copyright__ = 'Copyright (C) 2018 Devhouse Spindle'
+__license__ = 'MIT License'
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = ''
+
 setup(
-    name='django-logical-perms',
-    version='1.0',
-    description='Super awesome logical permissions for Django',
-    long_description='Base permissions upon logical methods and integrate them with Django.',
-    author='Robert de Vries',
-    author_email='meneer@kwieb.com',
+    name=__title__,
+    version=__version__,
+    description='Super awesome logical permissions for Django.',
+    long_description=long_description,
+    author=__author__,
+    author_email=__email__,
+    license=__license__,
     url='https://github.com/wearespindle/django-logical-perms',
     download_url='https://github.com/wearespindle/django-logical-perms.git',
-    license='MIT License',
     packages=[
         'django_logical_perms',
     ],
@@ -37,5 +50,5 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    ],
 )
